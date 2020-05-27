e5ea676ff89f1685a729035c6e1a8b6f2a7e243f
Josh <ison.84@buckeyemail.osu.edu>
2020-05-27 16:36:51 -0400

    Created title helper

--- sample_app/app/helpers/application_helper.rb
+++ sample_app/app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def full_title(title = '')
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    title.length.positive? ? title << ' | ' << base_title : base_title
+  end
 end
